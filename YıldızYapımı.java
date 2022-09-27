558cb13a5b2daabb4d1f43159dd3d0bdbd7480c8
semihgullboy <smhglby17@gmail.com>
2022-09-27 14:32:51 +0300

    Create YıldızYapımı.java

---
+++ YıldızYapımı.java
@@ -0,0 +1,31 @@
+package java101pratikler;
+
+import java.util.Scanner;
+
+public class YıldızYapımı {
+	public static void main(String[] args) {
+		 Scanner klavye = new Scanner(System.in);
+	        System.out.println("Bir Sayı Giriniz :");
+	        int n = klavye.nextInt();
+
+	        for (int i = 0; i <= n ; i++) {
+	            for (int j = 0; j < (n - i); j++) {
+	                System.out.print(" ");
+	            }
+	            for (int k = 1; k <= (2 * i + 1); k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println(" ");
+	        }
+	        for (int i = n-1; i>=0; i--) {
+	            for (int j = 1; j <= (n - i); j++) {
+	                System.out.print(" ");
+	            }
+	            for (int k = 1; k <= (2 * i + 1); k++) {
+	                System.out.print("*");
+	            }
+	            System.out.println(" ");
+	        }
+
+	}
+}
